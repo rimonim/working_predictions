4671c38b68f53be16be5b37e63ef68ebd96ced0e
Louis Teitelbaum <louis@teitelbaum.us>
2022-11-21 21:56:14 +0200

    Update model_comparisons.R

--- model_comparisons.R
+++ model_comparisons.R
@@ -16,6 +16,9 @@ d1$condition[d1$cue == "A" & d1$target == "Y"] <- "AY"
 d1$condition[d1$cue == "B" & d1$target == "X"] <- "BX"
 d1$condition[d1$cue == "B" & d1$target == "Y"] <- "BY"
 
+d1$p_cue[d1$cue == "A"] <- .8
+d1$p_cue[d1$cue == "B"] <- .2
+
 d1$p_global[d1$target == "X"] <- .68
 d1$p_global[d1$target == "Y"] <- .32
 
@@ -36,9 +39,15 @@ d1 <- d1 %>%
   rename(ID = jatosStudyResultId,
          probe = target,
          RT = response_time) %>%
-  select(ID, condition, cue, probe, p_global, p_conditional, correct, RT, correct) %>%
+  select(ID, condition, cue, probe, p_cue, p_global, p_conditional, correct, RT, correct) %>%
   ungroup()
 
+d1_agg <- d1 %>%
+  group_by(ID, condition, cue, probe, p_cue, p_global, p_conditional) %>%
+  summarise(RT = mean(RT),
+            correct = mean(correct)) %>%
+  mutate(p_posterior = p_cue*p_conditional) %>%
+  mutate(best_guess = round(p_conditional))
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 # Experiment 1.2 Data Cleanup
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
@@ -54,6 +63,10 @@ d2$condition[d2$cue == "A" & d2$target == "Y"] <- "AY"
 d2$condition[d2$cue == "B" & d2$target == "X"] <- "BX"
 d2$condition[d2$cue == "B" & d2$target == "Y"] <- "BY"
 d2$condition[d2$cue == "C"] <- "CX"
+
+d2$p_cue[d2$cue == "A"] <- .2
+d2$p_cue[d2$cue == "B"] <- .2
+d2$p_cue[d2$cue == "C"] <- .6
 
 d2$p_global[d2$target == "X"] <- .8
 d2$p_global[d2$target == "Y"] <- .2
@@ -76,32 +89,70 @@ d2 <- d2 %>%
   rename(ID = jatosStudyResultId,
          probe = target,
          RT = response_time) %>%
-  select(ID, condition, cue, probe, p_global, p_conditional, correct, RT) %>%
+  select(ID, condition, cue, probe, p_cue, p_global, p_conditional, correct, RT) %>%
   ungroup()
 
+d2_agg <- d2 %>%
+  group_by(ID, condition, cue, probe, p_cue, p_global, p_conditional) %>%
+  summarise(RT = mean(RT),
+            correct = mean(correct)) %>%
+  mutate(p_posterior = p_cue*p_conditional) %>%
+  mutate(best_guess = round(p_conditional))
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 # One Data Set to Rule them All 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
 d <- d1 %>% add_row(d2) %>%
-  mutate(p_posterior = p_global * p_conditional,
-         best_guess = round(p_conditional))
+  mutate(best_guess = round(p_conditional))
 
 # Aggregated by participant/condition
-# Aggregated by participant/condition
-  group_by(ID, condition, cue, probe, p_global, p_conditional, p_posterior, best_guess) %>%
+d_agg <- d %>%
+  group_by(ID, condition, cue, probe, p_cue, p_global, p_conditional, best_guess) %>%
   summarise(RT = mean(RT),
-            correct = mean(correct))
+            correct = mean(correct)) %>%
+  mutate(p_posterior = p_cue*p_conditional)
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 # Graphical Analysis
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
+d1_agg %>%
+  ggplot(aes(condition, RT, color = as.factor(ID), group = as.factor(ID))) +
+  geom_path(alpha = .8, size = 1) +
+  geom_point() +
+  scale_color_brewer(palette = "Paired") +
+  theme_minimal() +
+  theme(legend.position = "none") +
+  labs(title = "Experiment 1.1", x = "Condition", y = "Participant Mean RT (ms)")
+
+d2_agg %>%
+  ggplot(aes(condition, RT, color = as.factor(ID), group = as.factor(ID))) +
+  geom_path(alpha = .8, size = 1) +
+  geom_point() +
+  scale_color_brewer(palette = "Paired") +
+  theme_minimal() +
+  theme(legend.position = "none") +
+  labs(title = "Experiment 1.2", x = "Condition", y = "Participant Mean RT (ms)")
+
+
 # Do we see RTs separating into two clusters on some conditions?
 #  (this would support a threshold model)
 
-d1 %>% ggplot(aes(condition, RT)) +
-  geom_quasirandom(method = "pseudorandom", width = .3, alpha = .1)
+d1 %>% 
+  group_by(ID, condition, cue, probe, p_global, p_conditional) %>%
+  summarise(RT = mean(RT),
+            correct = mean(correct)) %>%
+  mutate(condition = factor(paste(cue, "→", probe), levels = c("A → X", "B → Y", "A → Y", "B → X"))) %>%
+  ggplot(aes(condition, RT)) +
+    geom_quasirandom(method = "pseudorandom", width = .2) + 
+    geom_pointrange(data = d1 %>% 
+                      mutate(condition = factor(paste(cue, "→", probe), levels = c("A → X", "B → Y", "A → Y", "B → X"))) %>%
+                      group_by(condition) %>% summarise(RTh = mean(RT) + sd(RT),
+                                                        RTl = mean(RT) - sd(RT),
+                                                        RT = mean(RT)),
+                    aes(y = RT, ymin = RTl, ymax = RTh), color = "orange") +
+    theme_minimal() +
+    labs(x = "Condition", y = "RT (ms)")
 
 d2 %>%   filter(condition != "CX") %>%
   ggplot(aes(condition, RT)) +
@@ -126,37 +177,132 @@ d2 %>%
 # Main conclusion: this is tricky to visualize.
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-# Model Comparisons
+# Model Comparisons: Aggregated by Participant/Condition
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
-# Model 0: LTM = Posterior Probability (Base Rate * Conditional)
+# Model 0: LTM = Conditional Probability
 # Slope is allowed to vary because maybe some people are more or less attuned to prediction in addition to being faster or slower generally
-mod_0 <- lmer(RT ~ 1 + p_posterior + (1 + p_posterior | ID), d)
-estimates(mod_0)
-visualize(mod_0, plot = "model", sample = 10)
+aggmod_0 <- lmer(RT ~ 1 + p_conditional + (1 + p_conditional | ID), d_agg)
+summary(aggmod_0)
+visualize(aggmod_0, plot = "model", jitter = .01, alpha = .8, sample = 34) +
+  labs(title = "Model 0 (Unified Dataset)") +
+  scale_x_continuous(breaks = c(.2, .33, .67, .8, 1), 
+                     labels = c("0.2\n(Exp1.1)", "0.33\n(Exp1.2)", "0.67\n(Exp1.2)", "0.8\n(Exp1.1)", "1.0\n(Exp1.2)"))
+
+aggmod_0.1 <- lmer(RT ~ 1 + p_conditional + (1 + p_conditional | ID), d1_agg)
+summary(aggmod_0.1)
+visualize(aggmod_0.1, plot = "model", alpha = .8, sample = 10)
+
+aggmod_0.2 <- lmer(RT ~ 1 + p_conditional + (1 + p_conditional | ID), d2_agg)
+summary(aggmod_0.2)
+visualize(aggmod_0.2, plot = "model", alpha = .8, sample = 10)
+
+# Model 1: Summed Parallel Predictions
+aggmod_1 <- lmer(RT ~ 1 + p_global + p_conditional + (1 + p_global + p_conditional | ID), d_agg)
+summary(aggmod_1)
+visualize(aggmod_1, plot = "model", formula = RT ~ p_conditional + ID | p_global, jitter = .01, alpha = .8, sample = 34) +
+  labs(title = "Model 1 (Unified Dataset)") +
+  scale_x_continuous(breaks = c(.2, .33, .67, .8, 1), labels = c("0.2\n(1.1)", "0.33\n(1.2)", "0.67\n(1.2)", "0.8\n(1.1)", "1.0\n(1.2)"))
+
+aggmod_1.1 <- lmer(RT ~ 1 + p_global + p_conditional + (1 + p_global + p_conditional | ID), d1_agg)
+summary(aggmod_1.1)
+visualize(aggmod_1.1, plot = "model", formula = RT ~ p_conditional + ID | p_global, jitter = .01, alpha = .8, sample = 34) +
+  labs(title = "Model 1 (Unified Dataset)") +
+  scale_x_continuous(breaks = c(.2, .33, .67, .8, 1), labels = c("0.2\n(1.1)", "0.33\n(1.2)", "0.67\n(1.2)", "0.8\n(1.1)", "1.0\n(1.2)"))
+
+aggmod_1.2 <- lmer(RT ~ 1 + p_global + p_conditional + (1 + p_global + p_conditional | ID), d2_agg)
+summary(aggmod_1.2)
+visualize(aggmod_1.2)
+
+# Model 2: Summed Proportional Stimulus-Response Associations
+aggmod_2 <- lmer(RT ~ 1 + p_global + p_posterior + (1 + p_global + p_posterior | ID), d_agg)
+summary(aggmod_2)
+visualize(aggmod_2, plot = "model", formula = RT ~ p_posterior | p_global, jitter = .01) +
+  labs(title = "Model 2 (Unified Dataset)") +
+  scale_x_continuous(breaks = c(.2*.2, .2*.33, .2*.67, .8*.2, .6, .8*.8),
+                     labels = c("BX(1.1)", "AY/BX(1.2)", "AX/BY(1.2)", "AY(1.1)", "CX(1.2)", "AX(1.1)"),
+                     guide = guide_axis(angle = 90, n.dodge = 2))
+
+aggmod_2.1 <- lmer(RT ~ 1 + p_global + p_posterior + (1 + p_global + p_posterior | ID), d1_agg)
+summary(aggmod_2.1)
+visualize(aggmod_2.1)
+
+aggmod_2.2 <- lmer(RT ~ 1 + p_global + p_posterior + (1 + p_global + p_posterior | ID), d2_agg)
+summary(aggmod_2.2)
+visualize(aggmod_2.2)
+
+# Model 3: LTM = Base Rate, WM = Conditional Best Guess
+aggmod_3 <- lmer(RT ~ 1 + p_global + best_guess + (1 + p_global + best_guess | ID), d_agg)
+summary(aggmod_3)
+visualize(aggmod_3, plot = "model", sample = 35) +
+  labs(title = "Model 3 (Unified Dataset)")
+
+aggmod_3.1 <- lmer(RT ~ 1 + p_global + best_guess + (1 + p_global + best_guess | ID), d1_agg)
+summary(aggmod_3.1)
+visualize(aggmod_3.1)
+
+aggmod_3.2 <- lmer(RT ~ 1 + p_global + best_guess + (1 + p_global + best_guess | ID), d2_agg)
+summary(aggmod_3.2)
+visualize(aggmod_3.2)
+
+# Model 4: LTM = Associative Model, WM = Conditional Best Guess
+aggmod_4 <- lm(RT ~ p_global + p_posterior + best_guess, d_agg)
+summary(aggmod_4)
+visualize(aggmod_4)
+
+aggmod_4.1 <- lm(RT ~ p_global + p_posterior + best_guess, d1_agg)
+summary(aggmod_4.1)
+visualize(aggmod_4.1)
+
+aggmod_4.2 <- lm(RT ~ p_global + p_posterior + best_guess, d2_agg)
+summary(aggmod_4.2)
+visualize(aggmod_4.2)
+
+# WM has a huge positive effect here, which seems wrong. It looks like it's inflating the effect of p_conditional and making up for it with best_guess
 
 
-mod_0.1 <- lmer(RT ~ 1 + p_global + p_conditional + (1 + p_global + p_conditional | ID), d)
-estimates(mod_0.1)
-visualize(mod_0.1, plot = "model", formula = RT ~ p_conditional + ID | p_global, sample = 10)
+# Model 5: LTM = Associative Model, WM = Conditional Load Threshold (NOW FOR THE HARD PART)
+# Useful resources: https://stat.ethz.ch/pipermail/r-sig-mixed-models/2007q4/000472.html
+#                   https://discourse.mc-stan.org/t/piecewise-linear-mixed-models-with-a-random-change-point/5306/15
 
-# Model 1: LTM = Base Rate, WM = Conditional Best Guess
-mod_1 <- lmer(RT ~ 1 + p_global + best_guess + (1 + p_global + best_guess | ID), d)
-estimates(mod_1)
-visualize(mod_1, plot = "model", sample = 10)
+# fit naive nonlinear model (so that we can give multilevel model reasonable starting points for coefficients)
+# the logistic function is a practical alternative to a true step function, which spells trouble for gradient descent
+# multstart introduces some randomness to avoid converging on local maxima and to help with converging in general
+naive_mod_3 <- nls.multstart::nls_multstart(RT ~ b0 + b1*p_global + b2*p_conditional + b3*plogis(100*(p_conditional + brk)),
+                                            data = d,
+                                            start_lower = c(b0 = 100, b1 = -500, b2 = -500, b3 = -500, brk  = -2),
+                                            start_upper = c(b0 = 1000, b1 = 500, b2 = 500, b3 = 500, brk  = 2),
+                                            iter = 500,
+                                            supp_errors = "Y")
 
-# Model 2: LTM = Associative Model, WM = Conditional Best Guess
-mod_2 <- lmer(RT ~ 1 + p_global + p_conditional + best_guess 
-              + (1 + p_global + p_conditional + best_guess | ID), d)
-summary(mod_2)
-visualize(mod_2, plot = "model", 
-          formula = RT ~ p_conditional + ID | best_guess, sample = 7)
-    # WM has a huge positive effect here, which seems wrong. It looks like it's inflating the effect of p_conditional and making up for it with best_guess
+summary(naive_mod_3)
+# Again WM has a huge positive effect here, which seems wrong. Things are not looking good for binary boosts in general.
+# Nevertheless, I'll start letting only threshold vary by participant
+mod_3 <- nlme::nlme(RT ~ b0 + b1*p_global + b2*p_conditional + b3*plogis(100*(p_conditional + brk)),
+                    data = d,
+                    fixed = b0 + b1 + b2 + b3 + brk ~ 1,
+                    random = brk ~ 1,
+                    groups = ~ ID,
+                    start = coef(naive_mod_3))
 
-model.comparison(mod_0, mod_1) # model 0 wins
-model.comparison(mod_0, mod_2) # model 0 wins
-model.comparison(mod_0, mod_0.1) # model 0.1 (plain spreading activation) is best
+summary(mod_3)
 
+coef(mod_3) %>%
+  mutate(ID = sort(unique(d$ID))) %>%
+  right_join(tibble(ID = sample(d$ID),
+                    p_global = seq(0, 1, length.out = nrow(d)),
+                    p_conditional = seq(0, 1, length.out = nrow(d)))) %>%
+  mutate(RT = b0 + b1*p_global + b2*p_conditional + b3*plogis(100*(p_conditional + brk))) %>%
+  ggplot(aes(p_conditional, RT)) +
+  geom_line(aes(group = ID)) +
+  geom_quasirandom(aes(p_conditional, RT), data = d, alpha = .1)
+
+# Model 6: LTM = Associative Model, WM = LTM Load Threshold
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+# Model Comparisons: Using all the data?
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
 # Model 3: LTM = Associative Model, WM = Conditional Load Threshold (NOW FOR THE HARD PART)
   # Useful resources: https://stat.ethz.ch/pipermail/r-sig-mixed-models/2007q4/000472.html
